9bc7315792feeed9eb3741eaeaf0d2910c262b0d
Ricardo Rodriguez <rodriguez.cando.ricardo.jamie@iisgalvanimi.edu.it>
2021-05-03 09:51:12 +0000

    server ricerca place fatto

--- ServerPlace/routes/place.js
+++ ServerPlace/routes/place.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+
+const uri = 'mongodb+srv://Lorenzo:casada11@learnandtravel.qzfpb.mongodb.net/LearnAndTravel?retryWrites=true&w=majority'
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/:place', function (req, res, next) {
+    console.log(req.params); //Leggo i parametri passati all'url
+    p = req.params.place;
+
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(searchPlace);
+        
+        function searchPlace(err){
+            if (err) console.log("connesione al db non riuscita");
+            else{
+                const collection = client.db("LearnAndTravel").collection("Places");
+                collection.find({"Città": p}).toArray(callBackQuery);
+            }
+
+        }  
+        function callBackQuery(err, result){
+            if (err) console.log(err.message);
+            else res.send(result);
+            client.close();
+        }
+}); 
+
+
+
+
 
 module.exports = router;
